e5b5915503e31aa7dcd4cedf6409d3a0286c7452
Tsega Yaekob <tsegayaekob7@gmail.com>
2023-06-07 12:56:35 +0300

    clean code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,141 +1,151 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Home from './components/Home';
-import Trips from './components/Trips'
-import TourGuide from './components/TourGuide';
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp';
-import NavBar from './components/NavBar';
-import Hotels from './components/Hotels';
-import HotelEach from './components/HotelEach';
-import Location from './components/Location';
-import Restaurant from './components/Restaurant'
+import Home from "./components/Home";
+import Trips from "./components/Trips";
+import TourGuide from "./components/TourGuide";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import NavBar from "./components/NavBar";
+import Hotels from "./components/Hotels";
+import HotelEach from "./components/HotelEach";
+import Location from "./components/Location";
+import Restaurant from "./components/Restaurant";
 
-import footerList from './db.json';
-import eachHotels from './db7.json';
-import attractions from './db5.json';
-
+import footerList from "./db.json";
 
 function App() {
+	const [locationType, setLocationType] = useState([]);
+	const [hotel, setHotel] = useState([]);
+	const [location, setLocation] = useState([]);
+	const [activity, setActivity] = useState([]);
+	const [restaurant, setRestaurant] = useState([]);
+	const [restaurantBook, setRestaurantBook] = useState([]);
+	const [meal, setMeal] = useState([]);
+	const [hotelRoom, setHotelRoom] = useState([]);
+	const [travel, setTravel] = useState([]);
+	const [tourGuide, setTourGuide] = useState([]);
+	const [signup, setSignup] = useState([]);
+	const [login, setLogin] = useState([]);
 
-  const [locationType, setLocationType] = useState([]);
-  const [hotel, setHotel] = useState([]);
-  const [location, setLocation] = useState([]);
-  const [activity, setActivity] = useState([]);
-  const [restaurant, setRestaurant] = useState([]);
-  const [restaurantBook, setRestaurantBook] = useState([]);  
-  const [meal, setMeal] = useState([]);  
-  const [hotelRoom, setHotelRoom] = useState([]);
-  const [travel, setTravel] = useState([]);
-  const [tourGuide, setTourGuide] = useState([]);
-  const [signup, setSignup] = useState([]);  
-  const [login, setLogin] = useState([]);
- 
-  // Fetching data from the database
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('/api/v1/locationtype');
-      const { locationTypes } = response.data.data;
-      setLocationType(locationTypes);
+	// Fetching data from the database
+	const fetchData = async () => {
+		try {
+			const response = await axios.get("/api/v1/locationtype");
+			const { locationTypes } = response.data.data;
+			setLocationType(locationTypes);
 
-      const response2 = await axios.get('/api/v1/locations');
-      const {locations} = response2.data.data;
-      setLocation(locations)
+			const response2 = await axios.get("/api/v1/locations");
+			const { locations } = response2.data.data;
+			setLocation(locations);
 
-      const response3 = await axios.get('/api/v1/hotels');
-      const {hotels} = response3.data.data;
-      setHotel(hotels)
+			const response3 = await axios.get("/api/v1/hotels");
+			const { hotels } = response3.data.data;
+			setHotel(hotels);
 
-      const response4 = await axios.get('/api/v1/hotelRoom');
-      const {hotelRooms} = response4.data.data;
-      setHotelRoom(hotelRooms);
+			const response4 = await axios.get("/api/v1/hotelRoom");
+			const { hotelRooms } = response4.data.data;
+			setHotelRoom(hotelRooms);
 
+			const response5 = await axios.get("/api/v1/activities");
+			const { activities } = response5.data.data;
+			setActivity(activities);
 
-      const response5 = await axios.get('/api/v1/activities');
-      const {activities} = response5.data.data;
-      setActivity(activities)
+			const response6 = await axios.get("/api/v1/restaurants");
+			const { restaurants } = response6.data.data;
+			setRestaurant(restaurants);
 
-      const response6 = await axios.get('/api/v1/restaurants');
-      const {restaurants} = response6.data.data;
-      setRestaurant(restaurants)
+			// const response9 = await axios.post('/api/v1/restaurantBooking');
+			// const {restaurantBook} = response9.data.data;
+			// setRestaurantBook(restaurantBook)
 
-      // const response9 = await axios.post('/api/v1/restaurantBooking');
-      // const {restaurantBook} = response9.data.data;
-      // setRestaurantBook(restaurantBook)      
+			const response7 = await axios.get("/api/v1/travel");
+			const { travels } = response7.data.data;
+			setTravel(travels);
 
-      const response7 = await axios.get('/api/v1/travel');
-      const {travels} = response7.data.data;
-      setTravel(travels)
+			const response8 = await axios.get("/api/v1/tourGuides");
+			const { tourGuides } = response8.data.data;
+			setTourGuide(tourGuides);
 
-      const response8 = await axios.get('/api/v1/tourGuides');
-      const {tourGuides} = response8.data.data;
-      setTourGuide(tourGuides)
+			// const response10 = await axios.post('/api/v1/users/signup');
+			// const {signup} = response10.data.data;
+			// setSignup(signup)
 
-      // const response10 = await axios.post('/api/v1/users/signup');
-      // const {signup} = response10.data.data;
-      // setSignup(signup)
+			// const response11 = await axios.post('/api/v1/users/login');
+			// const {login} = response11.data.data;
+			// setLogin(login)
 
-      // const response11 = await axios.post('/api/v1/users/login');
-      // const {login} = response11.data.data;
-      // setLogin(login)   
-      
-      const response12 = await axios.get('/api/v1/meals/');
-      const {meals} = response12.data.data;
-      setMeal(meals)
-     
-      // console.log(restaurants)
-    } catch (error) {
-      console.error('Error fetching location types:', error);
-    }
-  };
-  
-  useEffect(() => {
-    fetchData();
-  }, []);
-  
+			const response12 = await axios.get("/api/v1/meals/");
+			const { meals } = response12.data.data;
+			setMeal(meals);
 
-  return (
-    <div className="App">
+			// console.log(restaurants)
+		} catch (error) {
+			console.error("Error fetching location types:", error);
+		}
+	};
 
-      <Router>
-        <div className='content'>
-          <Switch>
-          <Route exact path='/'>
-              <NavBar/>
-              <Home data={locationType} explore={location} popular={activity} footer={footerList}/>
-            </Route>
-            <Route exact path="/location/:itemId" >
-              <Location locations={location}  activities={activity}  restaurants={restaurant} hotels={hotel}/>
-            </Route>
-            <Route exact path='/trips'>
-              <Trips data={locationType} hotels={hotel} hotelRooms={hotelRoom} restaurants={restaurant} travels={travel} tourGuides={tourGuide}/>
-            </Route>
-            <Route exact path='/tourGuides'>
-              <TourGuide/>
-            </Route>
-            <Route exact path='/signin'>
-              <SignIn/>
-            </Route>
-            <Route exact path='/signup'>
-              <SignUp/>
-            </Route>
-            <Route exact path='/hotels'>
-              <Hotels hotels={hotel} hotelRooms={hotelRoom}/>
-            </Route>
-            <Route exact path='/eachhotel/:itemId'>
-              <HotelEach items={eachHotels} hotelRooms={hotelRoom} />
-            </Route>
-            <Route exact path='/restaurant'>
-              <Restaurant restaurants={restaurant} meals={meal} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<div className='App'>
+			<Router>
+				<div className='content'>
+					<Switch>
+						<Route exact path='/'>
+							<NavBar />
+							<Home
+								data={locationType}
+								explore={location}
+								popular={activity}
+								footer={footerList}
+							/>
+						</Route>
+						<Route exact path='/location/:itemId'>
+							<Location
+								locations={location}
+								activities={activity}
+								restaurants={restaurant}
+								hotels={hotel}
+							/>
+						</Route>
+						<Route exact path='/trips'>
+							<Trips
+								data={locationType}
+								hotels={hotel}
+								hotelRooms={hotelRoom}
+								restaurants={restaurant}
+								travels={travel}
+								tourGuides={tourGuide}
+							/>
+						</Route>
+						<Route exact path='/tourGuides'>
+							<TourGuide />
+						</Route>
+						<Route exact path='/signin'>
+							<SignIn />
+						</Route>
+						<Route exact path='/signup'>
+							<SignUp />
+						</Route>
+						<Route exact path='/hotels'>
+							<Hotels hotels={hotel} hotelRooms={hotelRoom} />
+						</Route>
+						<Route exact path='/eachhotel/:itemId'>
+							<HotelEach hotelRooms={hotelRoom} />
+						</Route>
+						<Route exact path='/restaurant'>
+							<Restaurant restaurants={restaurant} meals={meal} />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
